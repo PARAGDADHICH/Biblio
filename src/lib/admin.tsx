c881b450a6419f98cf8520d4dfbda38c7f09858e
Parag_Dadhich <iit2022036@iiitl.ac.in>
2024-08-07 09:21:25 +0530

    changed serviceaccount in admin.tsx

--- src/lib/admin.tsx
+++ src/lib/admin.tsx
@@ -1,5 +1,19 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './../../firebase_admin_creds.json';
+// import * as serviceAccount from './../../firebase_admin_creds.json';
+
+const serviceAccount = {
+  type: process.env.NEXT_PUBLIC_TYPE,
+  project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.NEXT_PUBLIC_UNIVERSE_DOMAIN,
+};
 
 
 try {
